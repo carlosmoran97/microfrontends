83b199f1cbc637c76a7967b54fa39ce732d89670
Carlos Moran <cmoran@applaudostudios.com>
2021-11-25 10:08:06 -0600

    async load scripts

--- products/src/index.js
+++ products/src/index.js
@@ -1,11 +1,1 @@
-import faker from 'faker';
-
-let products = '';
-
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
-}
-
-const root = document.querySelector("#dev-products");
-root.innerHTML = products;
+import('./bootstrap');
